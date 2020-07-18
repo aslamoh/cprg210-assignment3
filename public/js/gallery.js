64036c0ebb057829f274c538b67d229a660b06c1
aslamoh <smaslam2018@gmail.com>
2020-07-18 14:39:07 -0600

    modified gallery.js

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -79,8 +79,6 @@ fetch('https://aslam-cprg210-assignment.herokuapp.com/api/destinations')
 	.then(function(data) {
 		const imgList = data;
 
-		console.log(imageList);
-
 		// Variable where we will store our img tags
 		let imgTemplate = '';
 
